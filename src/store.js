a51c775fdf2ec327b416102e51ec5124dc146469
minjie.zhao <minjie.zhao@hand-china.com>
2019-03-11 21:40:10 +0800

    [ADD]State & Getter & Mutation

--- src/store.js
+++ src/store.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 如果在模块化构建系统中，请确保在开头调用了 Vue.use(Vuex)
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    weather: [
+      { day: 'monday', weather: 'sunday' },
+      { day: 'tuesday', weather: 'sunday' },
+      { day: 'wednesday', weather: 'rainy' },
+      { day: 'thursday', weather: 'fog' }
+      // ...
+    ]
+  },
+  // 可以认为是 store 的计算属性
+  getters: {
+    // Getter 接受 state 作为其第一个参数：
+    sunday (state) {
+      return state.weather.filter(d => d.weather === 'sunday')
+    },
+    // Getter 也可以接受其他 getter 作为第二个参数 => 意义不明，测试发现getters指向getters对象
+    sundayButNotMonday (state, getters) {
+      return getters.sunday.filter(d => d.day !== 'monday')
+    },
+    // 通过让 getter 返回一个函数，来实现给 getter 传参
+    getTodayWeather (state) {
+      return (day) => state.weather.find(d => d.day === day).weather
+    }
+    // getTodayWeather: (state) => (day) => state.weather.find(d => d.day === day).weather
   },
   mutations: {
-
-  },
-  actions: {
-
+    // 提交载荷（Payload）
+    itRains (state, obj) {
+      state.weather.find(w => w.day === obj.day).weather = 'rainy'
+      // return 'riany'  测试发现 mutations 无法返回执行结果 => 个人推测只变更状态
+      //                                                      之后根据响应式数据动态刷新页面就是其目的
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div> today: {{ getDay }} <br> weather: {{ getTodayWeather }} {{ getTodayWeatherByPassingDay() }} </div>
+      <p>天气预报: </p>
+      <ul>
+        <li v-for="(v, i) in getWeather" :key="i">
+          {{ v.day }} - {{ v.weather }}
+        </li>
+      </ul>
+      <p>Sunday this week: </p>
+      <ul>
+        <li v-for="(d, i) in getSundayDays" :key="i">
+          {{ d.day }}
+        </li>
+      </ul>
+      <p>Sunny but not monday:  </p>
+      <ul>
+        <li v-for="(d, i) in getSundayButNotMonday" :key="i">
+          {{ d.day }}
+        </li>
+      </ul>
+      <button @click="thuesdayGetRain">天气变更预警：周四下雨 （注意周四天气）</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      day: 'monday'
+    }
+  },
+  computed: {
+    // 通过 store 实例获取getters
+    getSundayDays () {
+      return this.$store.getters.sunday
+    },
+    getSundayButNotMonday () {
+      return this.$store.getters.sundayButNotMonday
+    },
+    ...mapState({
+      // 等价于 getWeather: 'weahter'
+      getWeather: state => state.weather,
+
+      // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+      // test: state => {
+      //   console.log(this) // undefined
+      //   return 'ahahahah'
+      // }
+      getDay (state) {
+        return this.day // this == current component
+      },
+      getTodayWeather (state) {
+        return state.weather.find(d => d.day === this.day).weather
+      }
+    })
+  },
+  methods: {
+    // call getters by methods
+    // 注意，getter 在通过方法访问时，每次都会去进行调用，而不会缓存结果。
+    getTodayWeatherByPassingDay () {
+      return this.$store.getters.getTodayWeather(this.day)
+    },
+    thuesdayGetRain () {
+      // this.$store.commit('itRains', 'thursday')
+      // 对象风格的提交，整个对象都作为载荷传给 mutation 函数
+      this.$store.commit({
+        type: 'itRains',
+        day: 'thursday'
+      })
+    }
   }
 }
 </script>
+
+<style lang="scss">
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+p {
+  font-weight: bold;
+}
+</style>
